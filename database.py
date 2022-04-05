7f6753e278dc6d0d72c3153cf82852c40ef80203
John Boldyrev <62275093+johny322@users.noreply.github.com>
2022-04-05 15:34:37 +0300

    Delete database.py

--- database.py
+++
@@ -1,8 +0,0 @@
-# from pyzbar import pyzbar
-# import cv2
-# import qrcode
-#
-# async decode_qr(update: Update, context: CallbackContext):
-#     chat_id = update.message.chat_id
-#     if update.message.photo:
-#         id_img = update.message.photo[-1].file_id
